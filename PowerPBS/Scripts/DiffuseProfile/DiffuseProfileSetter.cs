16ed850694cf5f0518586a5e4dbd5c6a97b3416b
ken <redcool007@qq.com>
2021-09-02 14:52:16 +0800

    update namespace

--- PowerPBS/Scripts/DiffuseProfile/DiffuseProfileSetter.cs
+++ PowerPBS/Scripts/DiffuseProfile/DiffuseProfileSetter.cs
@@ -1,28 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[ExecuteInEditMode]
-public class DiffuseProfileSetter : MonoBehaviour
+namespace PowerPBS
 {
-    public Color strength = Color.white;
-    public Color falloff = Color.red;
-    private void OnEnable()
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    [ExecuteInEditMode]
+    public class DiffuseProfileSetter : MonoBehaviour
     {
-        SendKernels();
-    }
-    public void SendKernels()
-    {
-        var kernels = new List<Vector4>();
-        SSSSKernel.CalculateKernel(kernels, 25, strength, falloff);
-        Shader.SetGlobalVectorArray("_Kernel",kernels);
-    }
+        public Color strength = Color.white;
+        public Color falloff = Color.red;
+        private void OnEnable()
+        {
+            SendKernels();
+        }
+        public void SendKernels()
+        {
+            var kernels = new List<Vector4>();
+            SSSSKernel.CalculateKernel(kernels, 25, strength, falloff);
+            Shader.SetGlobalVectorArray("_Kernel", kernels);
+        }
 
 #if UNITY_EDITOR
-    private void Update()
-    {
-        SendKernels();
-    }
+        private void Update()
+        {
+            SendKernels();
+        }
 #endif
 
+    }
 }
