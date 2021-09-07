921051220dd7f527076390aa9de79159455d4a77
ken <redcool007@qq.com>
2021-09-07 11:17:26 +0800

    update alpha blend styles

--- PowerPBS/Editor/PowerShaderInspector.cs
+++ PowerPBS/Editor/PowerShaderInspector.cs
@@ -176,8 +176,8 @@ namespace PowerPBS
             EditorGUI.BeginChangeCheck();
             GUILayout.BeginVertical("");
             EditorGUILayout.Space(10);
-            EditorGUILayout.LabelField("Alpha Blend",EditorStyles.boldLabel);
-                presetBlendMode = (PresetBlendMode)EditorGUILayout.EnumPopup(ConfigTool.Text(propNameTextDict,"PresetBlendMode"), presetBlendMode);
+            GUILayout.Label("Alpha Blend", EditorStyles.boldLabel);
+            presetBlendMode = (PresetBlendMode)EditorGUILayout.EnumPopup(ConfigTool.Text(propNameTextDict, "PresetBlendMode"), presetBlendMode);
             GUILayout.EndVertical();
             if (EditorGUI.EndChangeCheck())
             {
