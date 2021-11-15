22573dc964d5f6f314c66a0ec7209a2ae831b4da
ken <redcool007@qq.com>
2021-11-15 16:38:02 +0800

    add ns

--- PowerPBS/Editor/Drawer/LiteToggle.cs
+++ PowerPBS/Editor/Drawer/LiteToggle.cs
@@ -4,22 +4,24 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-/// <summary>
-/// Toggle, no Keyword
-/// </summary>
-public class LiteToggle : MaterialPropertyDrawer
-{
-    public override void OnGUI(Rect position, MaterialProperty prop, string label, MaterialEditor editor)
+namespace PowerPBS{
+    /// <summary>
+    /// Toggle, no Keyword
+    /// </summary>
+    public class LiteToggle : MaterialPropertyDrawer
     {
-        bool value = prop.floatValue != 0;
-        EditorGUI.BeginChangeCheck();
-
-        EditorGUI.showMixedValue = prop.hasMixedValue;
-        value = EditorGUI.Toggle(position, label, value);
-        EditorGUI.showMixedValue = false;
-        if (EditorGUI.EndChangeCheck())
+        public override void OnGUI(Rect position, MaterialProperty prop, string label, MaterialEditor editor)
         {
-            prop.floatValue = value ? 1 : 0;
+            bool value = prop.floatValue != 0;
+            EditorGUI.BeginChangeCheck();
+
+            EditorGUI.showMixedValue = prop.hasMixedValue;
+            value = EditorGUI.Toggle(position, label, value);
+            EditorGUI.showMixedValue = false;
+            if (EditorGUI.EndChangeCheck())
+            {
+                prop.floatValue = value ? 1 : 0;
+            }
         }
     }
 }
