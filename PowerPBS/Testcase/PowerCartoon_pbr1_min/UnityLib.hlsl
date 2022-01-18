6f9aaa0d2f313f9afb7ec1ca5892aa930aae4b12
ken <redcool007@qq.com>
2022-01-18 19:06:04 +0800

    add powerCartoon

---
+++ PowerPBS/Testcase/PowerCartoon_pbr1_min/UnityLib.hlsl
@@ -0,0 +1,263 @@
+#if !defined(UNITY_LIB_HLSL)
+#define UNITY_LIB_HLSL
+
+#define TRANSFORM_TEX(tex, name) ((tex.xy) * name##_ST.xy + name##_ST.zw)
+
+
+#define HALF_MIN 6.103515625e-5  // 2^-14, the same value for 10, 11 and 16-bit: https://www.khronos.org/opengl/wiki/Small_Float_Formats
+#define HALF_MIN_SQRT 0.0078125  // 2^-7 == sqrt(HALF_MIN), useful for ensuring HALF_MIN after x^2
+
+
+float4 _MainLightPosition,_WorldSpaceLightPos0;
+half4 _MainLightColor,_LightColor0;
+
+#define _MainLightPosition _WorldSpaceLightPos0
+#define _MainLightColor _LightColor0
+
+float3 _WorldSpaceCameraPos;
+
+
+//==============================
+//  Transform
+//==============================
+float4x4 unity_ObjectToWorld;
+float4x4 unity_WorldToObject;
+#if !defined(USING_STEREO_MATRICES)
+float4x4 glstate_matrix_projection;
+float4x4 unity_MatrixV;
+float4x4 unity_MatrixInvV;
+float4x4 unity_MatrixInvP;
+float4x4 unity_MatrixVP;
+float4x4 unity_MatrixInvVP;
+float4 unity_StereoScaleOffset;
+int unity_StereoEyeIndex;
+#endif
+
+#define UNITY_MATRIX_M     unity_ObjectToWorld
+#define UNITY_MATRIX_I_M   unity_WorldToObject
+#define UNITY_MATRIX_V     unity_MatrixV
+#define UNITY_MATRIX_I_V   unity_MatrixInvV
+#define UNITY_MATRIX_P     (glstate_matrix_projection)
+#define UNITY_MATRIX_I_P   unity_MatrixInvP
+#define UNITY_MATRIX_VP    unity_MatrixVP
+#define UNITY_MATRIX_I_VP  unity_MatrixInvVP
+#define UNITY_MATRIX_MV    mul(UNITY_MATRIX_V, UNITY_MATRIX_M)
+#define UNITY_MATRIX_T_MV  transpose(UNITY_MATRIX_MV)
+#define UNITY_MATRIX_IT_MV transpose(mul(UNITY_MATRIX_I_M, UNITY_MATRIX_I_V))
+#define UNITY_MATRIX_MVP   mul(UNITY_MATRIX_VP, UNITY_MATRIX_M)
+
+float3 TransformObjectToWorld(float3 objectPos){
+    return mul(unity_ObjectToWorld,float4(objectPos,1)).xyz;
+}
+
+float3 TransformObjectToWorldDir(float3 objectDir){
+    return normalize( mul((float3x3)UNITY_MATRIX_M,objectDir) );
+}
+
+float4 TransformObjectToHClip(float3 objectPos){
+    return mul(UNITY_MATRIX_VP,mul(UNITY_MATRIX_M,float4(objectPos,1)));
+}
+
+float4 TransformWorldToHClip(float3 worldPos){
+    return mul(unity_MatrixVP,float4(worldPos,1));
+}
+
+float3 TransformObjectToWorldNormal(float3 normal){
+    return mul(float4(normal,1),UNITY_MATRIX_I_M).xyz;
+}
+
+float3 GetWorldSpaceViewDir(float3 worldPos){
+    return _WorldSpaceCameraPos - worldPos;
+}
+
+float3 GetWorldSpaceLightDir(float3 worldPos){
+    return _MainLightPosition.xyz;// - worldPos;
+}
+
+
+
+//==============================
+//  sh
+//==============================
+float4 unity_SHAr;
+float4 unity_SHAg;
+float4 unity_SHAb;
+float4 unity_SHBr;
+float4 unity_SHBg;
+float4 unity_SHBb;
+float4 unity_SHC;
+
+// Ref: "Efficient Evaluation of Irradiance Environment Maps" from ShaderX 2
+float3 SHEvalLinearL0L1(float3 N, float4 shAr, float4 shAg, float4 shAb)
+{
+    float4 vA = float4(N, 1.0);
+
+    float3 x1;
+    // Linear (L1) + constant (L0) polynomial terms
+    x1.r = dot(shAr, vA);
+    x1.g = dot(shAg, vA);
+    x1.b = dot(shAb, vA);
+
+    return x1;
+}
+
+float3 SHEvalLinearL2(float3 N, float4 shBr, float4 shBg, float4 shBb, float4 shC)
+{
+    float3 x2;
+    // 4 of the quadratic (L2) polynomials
+    float4 vB = N.xyzz * N.yzzx;
+    x2.r = dot(shBr, vB);
+    x2.g = dot(shBg, vB);
+    x2.b = dot(shBb, vB);
+
+    // Final (5th) quadratic (L2) polynomial
+    float vC = N.x * N.x - N.y * N.y;
+    float3 x3 = shC.rgb * vC;
+
+    return x2 + x3;
+}
+
+
+float3 SampleSH9(float4 SHCoefficients[7], float3 N)
+{
+    float4 shAr = SHCoefficients[0];
+    float4 shAg = SHCoefficients[1];
+    float4 shAb = SHCoefficients[2];
+    float4 shBr = SHCoefficients[3];
+    float4 shBg = SHCoefficients[4];
+    float4 shBb = SHCoefficients[5];
+    float4 shCr = SHCoefficients[6];
+
+    // Linear + constant polynomial terms
+    float3 res = SHEvalLinearL0L1(N, shAr, shAg, shAb);
+
+    // Quadratic polynomials
+    res += SHEvalLinearL2(N, shBr, shBg, shBb, shCr);
+
+    return res;
+}
+
+
+// Samples SH L0, L1 and L2 terms
+half3 SampleSH(half3 normalWS)
+{
+    // LPPV is not supported in Ligthweight Pipeline
+    float4 SHCoefficients[7];
+    SHCoefficients[0] = unity_SHAr;
+    SHCoefficients[1] = unity_SHAg;
+    SHCoefficients[2] = unity_SHAb;
+    SHCoefficients[3] = unity_SHBr;
+    SHCoefficients[4] = unity_SHBg;
+    SHCoefficients[5] = unity_SHBb;
+    SHCoefficients[6] = unity_SHC;
+
+    return max(half3(0, 0, 0), SampleSH9(SHCoefficients, normalWS));
+}
+
+//==============================
+//  ibl
+//==============================
+float3 DecodeHDREnvironment(float4 encodedIrradiance, float4 decodeInstructions)
+{
+    // Take into account texture alpha if decodeInstructions.w is true(the alpha value affects the RGB channels)
+    float alpha = max(decodeInstructions.w * (encodedIrradiance.a - 1.0) + 1.0, 0.0);
+
+    // If Linear mode is not supported we can skip exponent part
+    return (decodeInstructions.x * pow(alpha, decodeInstructions.y)) * encodedIrradiance.rgb;
+}
+
+
+
+//==============================
+//  lighting
+//==============================
+float D_GGXNoPI(float NdotH, float a2)
+{
+    float s = (NdotH * a2 - NdotH) * NdotH + 1.0;
+    return a2/ (s * s);
+}
+
+float MinimalistCookTorrance(float nh,float lh,float rough,float rough2){
+    float d = nh * nh * (rough2-1) + 1.00001f;
+    float lh2 = lh * lh;
+    float spec = rough2/((d*d) * max(0.1,lh2) * (rough*4+2)); // approach sqrt(rough2)
+    
+    #if defined (SHADER_API_MOBILE) || defined (SHADER_API_SWITCH)
+        spec = clamp(spec,0,100);
+    #endif
+    return spec;
+}
+
+float Pow4(float x){
+    float a = x*x;
+    return a*a;
+}
+
+//==============================
+//  Unpack from normal map
+//==============================
+half3 UnpackNormalRGB(half4 packedNormal, half scale = 1.0)
+{
+    half3 normal;
+    normal.xyz = packedNormal.rgb * 2.0 - 1.0;
+    normal.xy *= scale;
+    return normal;
+}
+
+half3 UnpackNormalRGBNoScale(half4 packedNormal)
+{
+    return packedNormal.rgb * 2.0 - 1.0;
+}
+
+half3 UnpackNormalAG(half4 packedNormal, half scale = 1.0)
+{
+    half3 normal;
+    normal.xy = packedNormal.ag * 2.0 - 1.0;
+    normal.z = max(1.0e-16, sqrt(1.0 - saturate(dot(normal.xy, normal.xy))));
+
+    // must scale after reconstruction of normal.z which also
+    // mirrors UnpackNormalRGB(). This does imply normal is not returned
+    // as a unit length vector but doesn't need it since it will get normalized after TBN transformation.
+    // If we ever need to blend contributions with built-in shaders for URP
+    // then we should consider using UnpackDerivativeNormalAG() instead like
+    // HDRP does since derivatives do not use renormalization and unlike tangent space
+    // normals allow you to blend, accumulate and scale contributions correctly.
+    normal.xy *= scale;
+    return normal;
+}
+
+// Unpack normal as DXT5nm (1, y, 0, x) or BC5 (x, y, 0, 1)
+half3 UnpackNormalmapRGorAG(half4 packedNormal, half scale = 1.0)
+{
+    // Convert to (?, y, 0, x)
+    packedNormal.a *= packedNormal.r;
+    return UnpackNormalAG(packedNormal, scale);
+}
+
+half3 UnpackNormal(half4 packedNormal)
+{
+#if defined(UNITY_ASTC_NORMALMAP_ENCODING)
+    return UnpackNormalAG(packedNormal, 1.0);
+#elif defined(UNITY_NO_DXT5nm)
+    return UnpackNormalRGBNoScale(packedNormal);
+#else
+    // Compiler will optimize the scale away
+    return UnpackNormalmapRGorAG(packedNormal, 1.0);
+#endif
+}
+
+half3 UnpackNormalScale(half4 packedNormal, half bumpScale)
+{
+#if defined(UNITY_ASTC_NORMALMAP_ENCODING)
+    return UnpackNormalAG(packedNormal, bumpScale);
+#elif defined(UNITY_NO_DXT5nm)
+    return UnpackNormalRGB(packedNormal, bumpScale);
+#else
+    return UnpackNormalmapRGorAG(packedNormal, bumpScale);
+#endif
+}
+half3 UnpackScaleNormal(half4 pn,half scale){
+    return UnpackNormalScale(pn,scale);
+}
+//============================
+#endif // UNITY_LIB_HLSL
