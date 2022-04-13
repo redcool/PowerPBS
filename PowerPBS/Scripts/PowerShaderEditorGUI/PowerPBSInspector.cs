89f1961903e6a99cd40c34343273920df3fdd8ae
ken <redcool007@qq.com>
2022-04-13 21:24:33 +0800

    update editorGui

---
+++ PowerPBS/Scripts/PowerShaderEditorGUI/PowerPBSInspector.cs
@@ -0,0 +1,17 @@
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PowerUtilities
+{
+    public class PowerPBSInspector : PowerShaderInspector
+    {
+        public PowerPBSInspector()
+        {
+            shaderName = "PowerPBS";
+        }
+
+    }
+}
+#endif
